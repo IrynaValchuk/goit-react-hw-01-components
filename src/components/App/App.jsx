4778d19bfb468ff67398993c455a1c35d4a5430c
Iryna <irula17ooo@gmail.com>
2023-04-09 23:16:02 +0300

    add profile

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import user from 'data/user.json';
+import { Profile } from 'components';
+import css from 'components/App/App.module.css';
+
+export const App = () => {
+  return (
+    <div className={css.container}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
