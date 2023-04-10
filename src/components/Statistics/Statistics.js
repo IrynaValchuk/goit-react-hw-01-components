cf40af0c70768a31d92486fa5fd0a96984876c12
Iryna <irula17ooo@gmail.com>
2023-04-10 10:32:27 +0300

    bugfix statistics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,7 +2,8 @@ import PropTypes from 'prop-types';
 import css from 'components/Statistics/Statistics.module.css';
 import {getRandomHexColor} from 'helpers/RandomColor'
 
-export const Statistics = ({ title, stats }) => (
+export const Statistics = ({ title, stats }) => {
+  return (
   <section className={css.statistics}>
     {title && <h2 className={css.title}>{title}</h2>}
 
@@ -17,7 +18,7 @@ export const Statistics = ({ title, stats }) => (
       ))}
     </ul>
   </section>
-);
+)};
 
 Statistics.protoTypes = {
   title: PropTypes.string.isRequired,
@@ -26,6 +27,6 @@ Statistics.protoTypes = {
       id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
-    })
-  ),
+    }).isRequired
+  ).isRequired,
 };
