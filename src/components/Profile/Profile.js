e114f749c8781dcf988a041b6be9ce7c4ee204d3
Iryna <irula17ooo@gmail.com>
2023-04-10 10:47:01 +0300

    bugfix profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
 import css from 'components/Profile/Profile.module.css'
 
-export const Profile = ({ username, tag, location, avatar, stats}) => (
+export const Profile = ({ username, tag, location, avatar, stats}) => {
+  return (
 <div className={css.profile}>
   <div className={css.description}>
     <img
@@ -29,7 +30,7 @@ export const Profile = ({ username, tag, location, avatar, stats}) => (
     </li>
   </ul>
 </div>
-)
+)}
 
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
@@ -40,5 +41,5 @@ Profile.propTypes = {
     followers: PropTypes.number.isRequired,
     views: PropTypes.number.isRequired,
     likes: PropTypes.number.isRequired,
-  })
+  }).isRequired
 }
