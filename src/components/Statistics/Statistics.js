dbc56b8255dbf1ccfac4aae61a6ca44ac0682108
Iryna <irula17ooo@gmail.com>
2023-04-10 23:22:14 +0300

    bugfix

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -24,7 +24,7 @@ export const Statistics = ({ title, stats }) => {
 };
 
 Statistics.protoTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
